ebe0bec41092eb76ccf937a8029fa9b472425d5c
Martin Povišer <povik@cutebit.org>
2024-01-27 19:11:18 +0100

    logic: Add debug, presynth script cli options
    Signed-off-by: Martin Povišer <povik@cutebit.org>

--- fold/logic/__main__.py
+++ fold/logic/__main__.py
@@ -20,9 +20,12 @@ def main():
                         help='source file names')
     parser.add_argument('-o', '--output', type=pathlib.Path,
                         default='/dev/stdout')
+    parser.add_argument('-g', '--debug', action='store_true', help='Enable Fold synthesis debug')
     parser.add_argument('-D', '--dump-immutlinks', type=pathlib.Path,)
     parser.add_argument('-d', '--dump-mutlinks', type=pathlib.Path,)
     parser.add_argument('-s', '--dump-immutlinks2', action='store_true',)
+    parser.add_argument('-i', '--presynth-cmds', type=str, help='Inject Yosys commands ahead of Fold synthesis')
+    parser.add_argument('-p', '--presynth-script', type=str, help='Run Yosys script ahead of Fold synthesis')
     parser.add_argument('-E', '--execid', help='Add ExecID instrumentation for full debugging',
                         action="store_true")
     parser.add_argument('-W', '--execid-width', help='Set width of ExecID tags',
@@ -66,12 +69,17 @@ def main():
 
     try:
         d.build()
+        d.rtl_module.set_top()
         d.rtl_module.ym.fixup_ports()
+        if args.presynth_cmds:
+            rtl.ys.run_pass(args.presynth_cmds, rtl_design.yd)
+        if args.presynth_script:
+            rtl.ys.run_frontend(args.presynth_script, "script", rtl_design.yd)
         if args.dump_immutlinks2:
             rtl.ys.run_pass("scratchpad -get immutlinks", rtl_design.yd)
         if not args.no_synth:
             rtl.ys.run_pass(f"plugin -i {str(pathlib.Path(__file__).resolve().parents[2])}/build/fold.so", rtl_design.yd)
-            rtl.ys.run_pass("fold_synth" + (" --execid" if args.execid else ""), rtl_design.yd)
+            rtl.ys.run_pass(f"{'debug ' if args.debug else ''}fold_synth" + (" --execid" if args.execid else ""), rtl_design.yd)
         rtl_design.write_il(args.output)
     except BadInput as e:
         print_code_snippet(e.markers)
